8f4c9cfc4c82f8c25b05c0820908c86479942dd6
ItsChavesCR <Achaba03x@gmail.com>
2024-05-11 23:21:35 -0600

    Avance 2

--- src/pages/NewPlace.tsx
+++ src/pages/NewPlace.tsx
@@ -10,30 +10,6 @@ const NewPlace = () => {
 
   const {countryId} = useContext(CountryContext)
 
-  let response;
-  try {
-    response = await fetch(`https://6632f68bf7d50bbd9b47beee.mockapi.io/countries/${countryId}/places`),
-      {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify(NewPlace),
-      }
-    if (!response.ok) throw new Error("Error to create product");
-  } catch (error) {
-    console.error("Error occurred while creating product: ", error);
-    throw error;
-  } finally {
-    console.log("Finished creating product");
-  }
-
-  try {
-    const responseData: PlaceType = await response.json();
-    return responseData;
-  } catch (error) {
-    console.error("Error occurred while parsing response: ", error);
-    throw error;
-  }
-}
 
   const { register, formState: { errors }, handleSubmit } = useForm();
 
